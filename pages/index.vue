1e78b275ce7826c28e6b1be5f4bced3c6b28e2e5
Marc Frankfurter <marcfrankfurter@gmail.com>
2023-04-24 19:51:41 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,4 @@
+<script setup></script>
+<template>
+  <div class="mb-3 text-xl">TEST</div>
+</template>
---
+++ pages/test/[value].vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const val = route.params.value;
+</script>
+
+<template>
+  <div>
+    <h1>Page: {{ val }}</h1>
+  </div>
+</template>
+
+<style scoped></style>
