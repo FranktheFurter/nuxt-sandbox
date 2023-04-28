089f67652fe01ce7286014709830ca354537eb59
Marc Frankfurter <Marc.Frankfurter@scheppach.com>
2023-04-28 12:41:35 +0200

    just trying some stuff

---
+++ pages/project/[id].vue
@@ -0,0 +1,14 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+import { useRoute } from "vue-router"
+import projects from "@/data/projects"
+const route = useRoute()
+const routeId = route.params.id as unknown as number
+const project = projects[routeId]
+</script>
+<template>
+  <div>
+    <h1>Catch all Projects</h1>
+    {{ project }}
+  </div>
+</template>
---
+++ pages/project/index.vue
@@ -0,0 +1,10 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+import projects from "@/data/projects"
+</script>
+<template>
+  <div>
+    <h1>Catch all Projects</h1>
+    {{ projects }}
+  </div>
+</template>
